cae77887129085af22daba88cd90d69943eb0306
Antonio Gilardi <antonio.gilardi@lbl.gov>
2021-12-14 09:37:53 -0800

    Adding Normalization

--- AdvancedML/Main_CrossVal.py
+++ AdvancedML/Main_CrossVal.py
@@ -4,6 +4,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import TimeSeriesSplit
 
 
 def normalize(data, norm_params, normalization_method="zscore"):
@@ -97,6 +98,7 @@ def windows_preprocessing(time_series, past_history, forecast_horizon):
         y.append(camera_series[j: j + forecast_horizon])
     return np.array(x), np.array(y)
 
+
 ## Loading Files 
 InOrOut = "OutLoop"
 #InOrOut = "InLoop" 
@@ -121,6 +123,8 @@ forecast_horizon = 1
 targetShift = -2
 fetureSelection = 0
 normalization_method = 'minmax'
+
+tscv = TimeSeriesSplit(gap=0, n_splits=5)
 
 # SHIFT
 if (targetShift!=0):
@@ -177,119 +181,108 @@ else:
     train = np.array([cam_train, phs_train, amp_train, diffPhs_train, diffAmp_train, Lphs_train, Lamp_train]) 
     test = np.array([cam_test, phs_test, amp_test, diffPhs_test, diffAmp_test, Lphs_test, Lamp_test])
 
-train, test, norm_params = normalize_dataset(train, test, normalization_method, dtype="float64")
 
-print("HI")
-print("------------------------------------------")
-print("")
-#Performing windowing with: Phs, Ampl, and Cam (X_train/test) to prefict Cam+1 (Y_train/test) 
-X_train, Y_train = windows_preprocessing(train, past_history, forecast_horizon)
-X_test, Y_test = windows_preprocessing(test, past_history, forecast_horizon)
-print("X -- train -->",np.shape(X_train))
-print("Y -- train -->",np.shape(Y_train))
-print("------------------------------------------")
-print("")
+FullDataset = np.concatenate(((train,test)),axis=1)
 
-#Saving dataset for hyperparameter tuning 
-np.save("X_train.npy",X_train)
-np.save("Y_train.npy",Y_train)
-np.save("X_test.npy",X_test)
-np.save("Y_test.npy",Y_test)
+metric_train = []
+metric_test = []
+print(list(tscv.split(FullDataset.transpose((1,0)))))
+for train_index, test_index in tscv.split(FullDataset.transpose((1,0))):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    train, test = FullDataset[:,train_index], FullDataset[:,test_index]
+    train, test, norm_params = normalize_dataset(train, test, normalization_method, dtype="float64")
+    X_train, Y_train = windows_preprocessing(train, past_history, forecast_horizon)
+    X_test, Y_test = windows_preprocessing(test, past_history, forecast_horizon)
 
+    #Reshape for RF model -- No temporal dimenison 
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
 
-# Reshape for RF model -- No temporal dimenison 
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
-print("X -- train -->",np.shape(X_train))
-print("Y -- train -->",np.shape(Y_train))
-print("------------------------------------------")
-print("")
-
-# Performing the ML
-#model = RandomForestRegressor(criterion='mse', n_jobs=-1, n_estimators=100,max_depth=6,min_samples_split=6,min_samples_leaf=7)
-#model = LinearRegression()
-model = MLPRegressor(hidden_layer_sizes=[32,16,8], random_state=1, max_iter=500)
-model.fit(X_train,Y_train)
-train_forecast = model.predict(X_train)
-Y_train_denorm = np.zeros(Y_train.shape)
-for i in range(Y_train.shape[0]):
+    #Performing the ML
+    # #model = RandomForestRegressor(criterion='mse', n_jobs=-1, n_estimators=100,max_depth=6,min_samples_split=6,min_samples_leaf=7)
+    # #model = LinearRegression()
+    model = MLPRegressor(hidden_layer_sizes=[32,16,8], random_state=1, max_iter=500)
+    model.fit(X_train,Y_train)
+    train_forecast = model.predict(X_train)
+    Y_train_denorm = np.zeros(Y_train.shape)
+    for i in range(Y_train.shape[0]):
         nparams = norm_params[0]
         train_forecast[i] = denormalize(train_forecast[i], nparams, normalization_method)
         Y_train_denorm[i] = denormalize(Y_train[i], nparams, normalization_method)
-train_forecast = np.squeeze(train_forecast)
-metrics_train = np.abs(train_forecast-np.squeeze(Y_train_denorm))
-test_forecast = model.predict(X_test)
-Y_test_denorm = np.zeros(Y_test.shape)
-for i in range(Y_test.shape[0]):
+    train_forecast = np.squeeze(train_forecast)
+    metrics_train = np.abs(train_forecast-np.squeeze(Y_train_denorm))
+    test_forecast = model.predict(X_test)
+    Y_test_denorm = np.zeros(Y_test.shape)
+    for i in range(Y_test.shape[0]):
         nparams = norm_params[0]
         test_forecast[i] = denormalize(test_forecast[i], nparams, normalization_method)
         Y_test_denorm[i] = denormalize(Y_test[i], nparams, normalization_method)
-test_forecast = np.squeeze(test_forecast)
-metrics_test = np.abs(test_forecast-np.squeeze(Y_test_denorm))
+    test_forecast = np.squeeze(test_forecast)
+    metrics_test = np.abs(test_forecast-np.squeeze(Y_test_denorm))
 
-print("")
-print("Training mean error",np.mean(metrics_train))
-print("Test mean error",np.mean(metrics_test))
-print("")
-print("Training RMSE",mean_squared_error(Y_train_denorm,train_forecast, squared=False))
-print("Test RMSE",mean_squared_error(Y_test_denorm, test_forecast, squared=False))
-print("")
+    metric_train.append(mean_squared_error(Y_train_denorm,train_forecast, squared=False))
 
+    metric_test.append(mean_squared_error(Y_test_denorm, test_forecast, squared=False))
 
-#Plotting
-fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6),sharey=True)
-ax1.plot(metrics_train,"+k")
-ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax1.hlines(np.mean(metrics_train), 0, len(metrics_train), colors='r', linestyles='--')
-ax1.set_ylabel("Metrics")
-ax1.set_xlabel("Samples")
-ax1.set_title("Train")
-ax2.plot(metrics_test,"+k")
-ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax2.hlines(np.mean(metrics_test), 0, len(metrics_test), colors='r', linestyles='--')
-ax2.set_xlabel("Samples")
-ax2.set_title("Test")
-plt.show()
+metric_train = [np.format_float_scientific(m, precision=2) for m in metric_train]
+print("Training RMSE",metric_train)
+metric_test = [np.format_float_scientific(m, precision=2) for m in metric_test]
+print("Test RMSE",metric_test)
 
-fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6),sharey=True)
-ax1.plot(train_forecast,np.squeeze(Y_train),"+k")
-ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax1.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
-ax1.set_ylabel("Label")
-ax1.set_xlabel("Prediction")
-ax1.set_title("Train")
-ax1.grid(axis="x")
-ax1.grid(axis="y")
-ax2.plot(test_forecast,np.squeeze(Y_test),"+k")
-ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax2.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
-ax2.set_xlabel("Prediction")
-ax2.set_title("Test")
-ax2.grid(axis="x")
-ax2.grid(axis="y")
-plt.show()
+# #Plotting
+# fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6),sharey=True)
+# ax1.plot(metrics_train,"+k")
+# ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax1.hlines(np.mean(metrics_train), 0, len(metrics_train), colors='r', linestyles='--')
+# ax1.set_ylabel("Metrics")
+# ax1.set_xlabel("Samples")
+# ax1.set_title("Train")
+# ax2.plot(metrics_test,"+k")
+# ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax2.hlines(np.mean(metrics_test), 0, len(metrics_test), colors='r', linestyles='--')
+# ax2.set_xlabel("Samples")
+# ax2.set_title("Test")
+# plt.show()
 
-massimo = max(np.max(Y_train_denorm),np.max(train_forecast),np.max(Y_test_denorm),np.max(test_forecast))
-minimo = min(np.min(Y_train_denorm),np.min(train_forecast),np.min(Y_test_denorm),np.min(test_forecast))
+# fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6),sharey=True)
+# ax1.plot(train_forecast,np.squeeze(Y_train),"+k")
+# ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax1.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+# ax1.set_ylabel("Label")
+# ax1.set_xlabel("Prediction")
+# ax1.set_title("Train")
+# ax1.grid(axis="x")
+# ax1.grid(axis="y")
+# ax2.plot(test_forecast,np.squeeze(Y_test),"+k")
+# ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax2.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+# ax2.set_xlabel("Prediction")
+# ax2.set_title("Test")
+# ax2.grid(axis="x")
+# ax2.grid(axis="y")
+# plt.show()
 
-fig, (ax1,ax2) = plt.subplots(2,figsize=(16,6))
-ax1.plot(np.squeeze(Y_train_denorm),"r",label= "Label")
-ax1.plot(train_forecast,"k",label= "Prediction")
-ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax1.set_ylabel("Centroid Error")
-ax1.set_title("Train")
-ax1.grid(axis="x")
-ax1.grid(axis="y")
-ax1.set_ylim((minimo, massimo))
-ax1.legend()
-ax2.plot(np.squeeze(Y_test_denorm),"r",label= "Label")
-ax2.plot(test_forecast,"k",label= "Prediction")
-ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-ax2.set_ylabel("Centroid Error")
-ax2.set_xlabel("Time")
-ax2.set_title("Test")
-ax2.grid(axis="x")
-ax2.grid(axis="y")
-ax2.set_ylim((minimo, massimo))
-ax2.legend()
-plt.show()
+# massimo = max(np.max(Y_train_denorm),np.max(train_forecast),np.max(Y_test_denorm),np.max(test_forecast))
+# minimo = min(np.min(Y_train_denorm),np.min(train_forecast),np.min(Y_test_denorm),np.min(test_forecast))
+
+# fig, (ax1,ax2) = plt.subplots(2,figsize=(16,6))
+# ax1.plot(np.squeeze(Y_train_denorm),"r",label= "Label")
+# ax1.plot(train_forecast,"k",label= "Prediction")
+# ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax1.set_ylabel("Centroid Error")
+# ax1.set_title("Train")
+# ax1.grid(axis="x")
+# ax1.grid(axis="y")
+# ax1.set_ylim((minimo, massimo))
+# ax1.legend()
+# ax2.plot(np.squeeze(Y_test_denorm),"r",label= "Label")
+# ax2.plot(test_forecast,"k",label= "Prediction")
+# ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+# ax2.set_ylabel("Centroid Error")
+# ax2.set_xlabel("Time")
+# ax2.set_title("Test")
+# ax2.grid(axis="x")
+# ax2.grid(axis="y")
+# ax2.set_ylim((minimo, massimo))
+# ax2.legend()
+# plt.show()

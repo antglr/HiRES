c91f09aaf49c8650d76c3c1f110bac580e99eae1
Antonio <antonio94gilardi@gmail.com>
2022-03-25 15:18:18 -0400

    Fixing a but in the testing

--- AdvancedML/Main_Net.py
+++ AdvancedML/Main_Net.py
@@ -64,8 +64,9 @@ def denormalize(data, norm_params, normalization_method="zscore"):
         return data * (norm_params["max"] - norm_params["min"]) + norm_params["min"]
     elif normalization_method == "None":
         return data
-def denormalization(X, Y, norm_params, normalization_method):
-    forecast = model(X).numpy()
+def denormalization(forecast, X, Y, norm_params, normalization_method):
+    #forecast2 = model(X).numpy()
+    #print(np.all(forecast2==forecast))
     Y_denorm = np.zeros(Y.shape)
     for j in range(Y.shape[0]):
         nparams = norm_params[0]
@@ -144,17 +145,25 @@ def plotting_2(train_forecast,Y_train,test_forecast,Y_test,i):
 def plotting_3(train_forecast,Y_train,test_forecast,Y_test,i, model):
     massimo = max(np.max(Y_train),np.max(train_forecast),np.max(Y_test),np.max(test_forecast))
     minimo = min(np.min(Y_train),np.min(train_forecast),np.min(Y_test),np.min(test_forecast))
+
+    diff_train = np.abs(Y_train - train_forecast)
+    diff_test = np.abs(Y_test - test_forecast)
+    RMS_train = np.sqrt(np.mean(Y_train**2))
+    RMSE_train = np.sqrt(np.mean((diff_train)**2))
+    RMS_test = np.sqrt(np.mean(Y_test**2))
+    RMSE_test = np.sqrt(np.mean((diff_test)**2))
+
     shift = 0
 
     fig, (ax1,ax2,ax3) = plt.subplots(3,figsize=(16,6))
-    ax1.plot(np.squeeze(Y_train),"r",label= "Label")
-    ax1.plot(train_forecast,"k",label= "Prediction")
-    S0 = np.std(np.squeeze(Y_train))
-    S1 = np.std(train_forecast)
-    S = np.std(np.abs(np.squeeze(Y_train)-train_forecast))
-    ax1.plot(S0, color='k', linestyle='-',label= "STD Data:{}".format(np.format_float_scientific(S0, precision=2)))
-    ax1.plot(S1, color='g', linestyle='-',label= "STD Prediction:{}".format(np.format_float_scientific(S, precision=2)))
-    ax1.plot(S, color='m', linestyle='-',label= "STD (|Data - Prediction|):{}".format(np.format_float_scientific(S, precision=2)))
+    ax1.plot(np.squeeze(Y_train[:250]),"r",label= "Label")
+    ax1.plot(train_forecast[:250],"k",label= "Prediction")
+    #S0 = np.std(np.squeeze(Y_train))
+    #S1 = np.std(train_forecast)
+    #S = np.std(np.abs(np.squeeze(Y_train)-train_forecast))
+    #ax1.plot(S0, color='k', linestyle='-',label= "STD Data:{}".format(np.format_float_scientific(S0, precision=2)))
+    ax1.plot(RMS_train, color='g', linestyle='-',label= "RMS:{}".format(np.format_float_scientific(RMS_train, precision=2)))
+    ax1.plot(RMSE_train, color='m', linestyle='-',label= "RMSE:{}".format(np.format_float_scientific(RMSE_train, precision=2)))
     ax1.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
     ax1.set_ylabel("Centroid Error")
     ax1.set_title("Train")
@@ -164,10 +173,12 @@ def plotting_3(train_forecast,Y_train,test_forecast,Y_test,i, model):
     ax1.legend()
     ax2.plot(np.squeeze(Y_test),"r",label= "Label")
     ax2.plot(np.roll(test_forecast,shift),"k",label= "Prediction")
-    S0 = np.std(np.squeeze(Y_test))
-    S1 = np.std(test_forecast)
-    ax2.plot(S0, color='k', linestyle='-',label= "STD Data:{}".format(np.format_float_scientific(S0, precision=2)))
-    ax2.plot(S1, color='g', linestyle='-',label= "STD Prediction:{}".format(np.format_float_scientific(S1, precision=2)))
+    #S0 = np.std(np.squeeze(Y_test))
+    #S1 = np.std(test_forecast)
+    #ax2.plot(S0, color='k', linestyle='-',label= "STD Data:{}".format(np.format_float_scientific(S0, precision=2)))
+    #ax2.plot(S1, color='g', linestyle='-',label= "STD Prediction:{}".format(np.format_float_scientific(S1, precision=2)))
+    ax2.plot(RMS_test, color='g', linestyle='-',label= "RMS:{}".format(np.format_float_scientific(RMS_test, precision=2)))
+    ax2.plot(RMSE_test, color='m', linestyle='-',label= "RMSE:{}".format(np.format_float_scientific(RMSE_test, precision=2)))
     ax2.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
     ax2.set_ylabel("Centroid Error")
     ax2.set_xlabel("Time")
@@ -176,9 +187,9 @@ def plotting_3(train_forecast,Y_train,test_forecast,Y_test,i, model):
     ax2.grid(axis="y")
     ax2.set_ylim((minimo, massimo))
     ax2.legend()
-    ax3.plot(np.abs(np.squeeze(Y_test) - np.roll(test_forecast,shift)), "b", label= "Label - Prediction")
-    S = np.std(np.abs(np.squeeze(Y_test)-test_forecast))
-    ax3.plot(S, color='m', linestyle='-',label= "STD (|Data - Prediction|):{}".format(np.format_float_scientific(S, precision=2)))
+    ax3.plot(np.squeeze(Y_test) - np.roll(test_forecast,shift), "b", label= "Label - Prediction")
+    #S = np.std(np.abs(np.squeeze(Y_test)-test_forecast))
+    #ax3.plot(S, color='m', linestyle='-',label= "STD (|Data - Prediction|):{}".format(np.format_float_scientific(S, precision=2)))
     ax3.ticklabel_format(axis="y", style="sci", scilimits=(0,0))   
     ax3.set_ylabel("|Label - Prediction|")
     ax3.set_xlabel("Time")
@@ -294,6 +305,9 @@ def testing(X_test, past_history, FullDataset):
             test_forecast.append(local_forecast)
     return test_forecast
 
+def root_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true))) 
+
 if __name__ == "__main__":
     t = time.time()
     percentage = 80 
@@ -314,12 +328,10 @@ if __name__ == "__main__":
     traintest_size = len(cam)//splitting_traintest
     split = int(traintest_size*percentage/100)
 
-    metric_train = []
-    metric_test = []
-    std_train = []
-    std_diff_train = []
-    std_test = []
-    std_diff_test = []
+    rms_train = []
+    rms_test = []
+    rmse_train = []
+    rmse_test = []
     for i in range(splitting_traintest):
         # Splitting dataset
         start_train = traintest_size*i
@@ -338,54 +350,48 @@ if __name__ == "__main__":
   
         # Model
         model = lstm()
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')       
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss = root_mean_squared_error)  
         callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5) 
-        train_forecast_pre = model(X_train).numpy()
-        history = model.fit(X_train,Y_train,batch_size=64, epochs=100, validation_data=(X_test,Y_test), shuffle=True)
+        history = model.fit(X_train,Y_train,batch_size=64, epochs=100, validation_split=0.1, shuffle=False)
+        train_forecast = model(X_train).numpy()
         # Test
         test_forecast = testing(X_test = X_test, past_history = past_history, FullDataset = FullDataset)
+        #test_forecast = model(X_test).numpy()
         # Denormalize
-        train_forecast, metrics_train, Y_train_denorm= denormalization(X=X_train, Y = Y_train, norm_params= norm_params, normalization_method= normalization_method)
-        test_forecast, metrics_test, Y_test_denorm= denormalization(X=X_test, Y = Y_test, norm_params= norm_params, normalization_method= normalization_method)
-
+        train_forecast, metrics_train, Y_train_denorm= denormalization(forecast= train_forecast, X=X_train, Y = Y_train, norm_params= norm_params, normalization_method= normalization_method)
+        test_forecast, metrics_test, Y_test_denorm= denormalization(forecast= test_forecast, X=X_test, Y = Y_test, norm_params= norm_params, normalization_method= normalization_method)
         # Metrics
         Y_train_denorm = np.squeeze(Y_train_denorm)
         Y_test_denorm = np.squeeze(Y_test_denorm)
-        std_train.append(np.std(Y_train_denorm))
-        std_test.append(np.std(Y_test_denorm))
-        diff_train = np.abs(np.squeeze(Y_train_denorm) - train_forecast)
-        diff_test = np.abs(np.squeeze(Y_test_denorm) - test_forecast)
-        std_diff_train.append(np.std(diff_train))
-        std_diff_test.append(np.std(diff_test))
-        metric_train.append([mean_squared_error(Y_train_denorm, train_forecast, squared=False), mean_absolute_percentage_error(Y_train_denorm, train_forecast)])
-        metric_test.append([mean_squared_error(Y_test_denorm, test_forecast, squared=False), mean_absolute_percentage_error(Y_test_denorm, test_forecast)])
+        diff_train = np.abs(Y_train_denorm - train_forecast)
+        diff_test = np.abs(Y_test_denorm - test_forecast)
+
+        rms_train.append(np.sqrt(np.mean(Y_train_denorm**2)))
+        rms_test.append(np.sqrt(np.mean(Y_test_denorm**2)))
+        rmse_train.append(np.sqrt(np.mean((diff_train)**2)))
+        rmse_test.append(np.sqrt(np.mean((diff_test)**2)))
+
         # Plotting
         plotting_1(history,i)
         plotting_2(train_forecast,Y_train_denorm,test_forecast,Y_test_denorm,i)
         plotting_3(train_forecast,Y_train_denorm,test_forecast,Y_test_denorm,i, model)
 
-    
     #Printing
-    sci_metric_train = [np.format_float_scientific(m[0], precision=2) for m in metric_train]
-    sci_std_train = [np.format_float_scientific(m, precision=2) for m in std_train]
-    sci_std_diff_train = [np.format_float_scientific(m, precision=2) for m in std_diff_train]
-    sci_metric_test = [np.format_float_scientific(m[0], precision=2) for m in metric_test]
-    sci_std_test = [np.format_float_scientific(m, precision=2) for m in std_test]
-    sci_std_diff_test = [np.format_float_scientific(m, precision=2) for m in std_diff_test]
+    sci_rms_train = [np.format_float_scientific(m, precision=2) for m in rms_train]
+    sci_rms_test = [np.format_float_scientific(m, precision=2) for m in rms_test]
+    sci_rmse_train = [np.format_float_scientific(m, precision=2) for m in rmse_train]
+    sci_rmse_test = [np.format_float_scientific(m, precision=2) for m in rmse_test]
     
     print("")
     print("-------------------------------------- TRAIN --------------------------------------")
-    print("STD RealDataser ----------------------------------->",sci_std_train)
-    print("STD (|RealDataser - Prediction|) -------------------->",sci_std_diff_train)
-    print("RMSE Prediction ----------------------------------->",sci_metric_train)
+    print("RMS Label ----------------------------------->",sci_rms_train)
+    print("RMSE  --------------------------------------->",sci_rmse_train)
     print("-----------------------------------------------------------------------------------")
     print("")
     print("")
     print("--------------------------------------- TEST ---------------------------------------")
-    print("STD RealDataser ----------------------------------->",sci_std_test)
-    print("STD (|RealDataser - Prediction|)  ------------------->",sci_std_diff_test)
-    print("RMSE Prediction ----------------------------------->",sci_metric_test)
+    print("RMS Label ----------------------------------->",sci_rms_test)
+    print("RMSE ---------------------------------------->",sci_rmse_test)
     print("-----------------------------------------------------------------------------------")
     elapsed = time.time() - t
-    print("-----------------------------------------------------------------------------------")
     print("TIME:",elapsed)
